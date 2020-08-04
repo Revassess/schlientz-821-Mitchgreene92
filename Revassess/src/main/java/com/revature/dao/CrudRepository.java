faacc0d31abad9b13d6465a39fbef48b6473d1ea
mitchgreene92 <mitchgreene92@gmail.com>
2020-08-04 18:03:36 -0500

    Spent 1 extra hour over since we had technical difficulties starting it...ran out of time while writing the CRUD Methods in the UserRepository

--- Revassess/src/main/java/com/revature/dao/CrudRepository.java
+++ Revassess/src/main/java/com/revature/dao/CrudRepository.java
@@ -1,5 +1,13 @@
 package com.revature.dao;
+import java.util.Set; 
 
 public interface CrudRepository {
 
+	public Object save(Object o);
+	public Set<Object> findAll();
+	public Object findById(Integer id);
+	public boolean update(Object o);
+	public boolean deleteById(Integer id);
+	
+	
 }
--- Revassess/src/main/java/com/revature/dao/UserRepository.java
+++ Revassess/src/main/java/com/revature/dao/UserRepository.java
@@ -1,5 +1,40 @@
 package com.revature.dao;
 
-public class UserRepository {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class UserRepository implements CrudRepository{
+	
+	//problem 1
+	private static ConnectionUtil cu = ConnectionUtil.getInstance();
+	private static Connection connection = cu.connect();
+	
+	//problem 4 - ran out of time after writing first implementation
+	public User save(Object u) {
+		User user = (User u);
+		
+		try {
+			
+			String sql = "INSERT INTO app_user VALUES(?,?,?,?,?,?);"
+			PreparedStatement ps = connection.prepareStatement(sql);
+			
+			ps.setString(1, Integer.toString(user.getUsername()));
+			ps.setString(2,  user.getUsername());
+			ps.setString(3,  user.getPassword());
+			ps.setString(4,  user.getFirstName());
+			ps.setString(5,  user.getLastName());
+			ps.setString(6,  Integer.toString(user.getRole()));
+			
+			ps.executeQuery();
+			
+			return user;
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 
 }
